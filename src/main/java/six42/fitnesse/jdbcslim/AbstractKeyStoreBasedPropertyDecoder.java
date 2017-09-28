6d08d1234f9e6b1e0daf6598c5d4c546b1baf663
Fried Hoeben <github@hsac.nl>
2017-09-28 21:50:35 +0200

    Introduce decoder base classes.

---
+++ src/main/java/six42/fitnesse/jdbcslim/AbstractKeyStoreBasedPropertyDecoder.java
@@ -0,0 +1,27 @@
+package six42.fitnesse.jdbcslim;
+
+import java.io.File;
+
+/**
+ * Base class for property decoder using key store file.
+ */
+public abstract class AbstractKeyStoreBasedPropertyDecoder extends AbstractPropertyDecoder {
+    protected boolean configureBasedOn(String key, String value) {
+        boolean removeKey = false;
+        if (key.equalsIgnoreCase(".keyStoreLocation")) {
+            if (value.equals("")) {
+                setKeyStore(null);
+            } else {
+                File keyStoreFile = new File(value);
+                setKeyStore(keyStoreFile);
+                if (isDebug()) {
+                    System.out.println("New Key Store Location " + keyStoreFile.getAbsolutePath());
+                }
+            }
+            removeKey = true;
+        }
+        return removeKey;
+    }
+
+    protected abstract void setKeyStore(File keyStore);
+}
