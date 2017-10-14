527f9e79ae0445167f278c8408a08775522eaa38
Fried Hoeben <github@hsac.nl>
2017-10-14 17:27:43 +0200

    Run userguide tests via jUnit instead of via command line (using standard 'test', instead of custom 'acceptanceTest')

---
+++ src/test/java/six42/fitnesse/jdbcslim/JdbcSlimAcceptanceTest.java
@@ -0,0 +1,11 @@
+package six42.fitnesse.jdbcslim;
+
+import fitnesse.junit.FitNesseRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(FitNesseRunner.class)
+@FitNesseRunner.FitnesseDir(".")
+@FitNesseRunner.OutputDir("target/fitnesse-results")
+@FitNesseRunner.Suite("PlugIns.JdbcSlim.UserGuide")
+public class JdbcSlimAcceptanceTest {
+}
