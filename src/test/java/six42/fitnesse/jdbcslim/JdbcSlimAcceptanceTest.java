75864cb6e521c3f963aea8d80ddf418be4aa81b1
six42 <six42@gmx.net>
2017-10-19 01:17:00 +0200

    added junit and jacoco reports

---
+++ src/test/java/six42/fitnesse/jdbcslim/JdbcSlimAcceptanceTest.java
@@ -0,0 +1,12 @@
+package six42.fitnesse.jdbcslim;
+
+import fitnesse.junit.FitNesseRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(FitNesseRunner.class)
+@FitNesseRunner.FitnesseDir(".")
+@FitNesseRunner.OutputDir("build/reports/fitnesse-results")
+@FitNesseRunner.Suite("PlugIns.JdbcSlim.UserGuide")
+//@FitNesseRunner.DebugMode(false)
+public class JdbcSlimAcceptanceTest {
+}
